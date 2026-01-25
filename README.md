654d4555d1bdc8843d23eab056420d2487efd40d
Maor Sharon <maor@zappy5.com>
2026-01-25 17:22:31 +0200

    Pre-publish sync: Fix paths for sub-pages

--- README.md
+++ README.md
@@ -70,7 +70,7 @@ This site is automatically deployed to Render as a static site serving from the 
 
 ## Generated on
 
-2026-01-23T16:17:32.290Z
+2026-01-25T15:22:07.622Z
 
 ---
 
--- script.js
+++ script.js
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const phoneHeaderBtn = document.querySelector('.phone-header-btn');
   if (phoneHeaderBtn) {
     phoneHeaderBtn.addEventListener('click', function() {
-      const phoneNumber = '[business_phone]';
+      const phoneNumber = '04-8669000';
       window.location.href = 'tel:' + phoneNumber;
     });
   }
